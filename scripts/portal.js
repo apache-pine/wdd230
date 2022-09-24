e26eab87a031008b3a0380d93a8fe537b638e750
apache-pine <kylar.sorensen@gmail.com>
2022-09-23 18:56:59 -0700

    Fixed meta desc, kabob-case tags and optimized img

--- scripts/portal.js
+++ scripts/portal.js
@@ -1,7 +1,7 @@
 const current_year = new Date().getFullYear();
 
-document.querySelector(".current_year").textContent = current_year;
+document.querySelector(".current-year").textContent = current_year;
 
 let last_modif = new Date(document.lastModified);
 
-document.querySelector("#last_modif").textContent = `Last Updated: ${last_modif.toLocaleString()}`;
+document.querySelector("#last-modif").textContent = `Last Updated: ${last_modif.toLocaleString()}`;
