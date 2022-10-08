71ddcdf67922cab6a0bf18960a5d0f868e3e4749
apache-pine <kylar.sorensen@gmail.com>
2022-10-07 23:04:36 -0700

    basic content and css

--- chamber/scripts/yakimacc.js
+++ chamber/scripts/yakimacc.js
@@ -5,3 +5,14 @@ document.querySelector(".current-year").textContent = current_year;
 let last_modif = new Date(document.lastModified);
 
 document.querySelector("#last-modif").textContent = `Last Updated: ${last_modif.toLocaleString()}`;
+
+let hamBtn = document.getElementById("hamBtn");
+
+let primaryNav = document.getElementById("primaryNav");
+
+function toggleMenu() {
+    primaryNav.classList.toggle("open")
+    hamBtn.classList.toggle("open")
+};
+
+hamBtn.onclick = toggleMenu;
