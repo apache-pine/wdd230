49c92eb0c5b405d292bb621043ecf9edae1ac515
apache-pine <kylar.sorensen@gmail.com>
2022-10-14 12:36:56 -0700

    added js and api for auto weather updates

--- lesson5/scripts/yakimacc.js
+++ lesson5/scripts/yakimacc.js
@@ -27,3 +27,61 @@ current_date.innerHTML = `<em>${full_date}</em>`;
 
 const current_day = now.getDay();
 
+const weather_link = "https://api.openweathermap.org/data/2.5/weather?q=yakima,wa,usa&appid=f2cfbb52b6e01d3767725b983a37e017&units=imperial"
+
+let the_hour = now.getHours();
+
+function getWeather(weather){
+	if(weather == "Haze"){
+        if(the_hour<18 && the_hour>6)
+            return "images/weather-icons/animated/cloudy-day-1.svg"
+        else
+            return "images/weather-icons/animated/cloudy-night-1.svg"
+    }
+    else if(weather == "Clouds"){
+        return "images/weather-icons/animated/cloudy.svg"
+    }
+    else if(weather == "Rain"){
+        return "images/weather-icons/animated/rainy-6.svg"
+    }
+    else if(weather == "Mist"){
+        return "images/weather-icons/animated/snowy-4.svg"
+    }
+    else if(weather == "Clear Sky"){
+        if(the_hour<18 && the_hour>6)
+            return "images/weather-icons/animated/day.svg"
+        else
+            return "images/weather-icons/animated/night.svg"
+    }
+    else if(weather == "Smoke"){
+        return "images/weather-icons/animated/snowy-6.svg"
+    }
+    else if(weather == "Drizzle"){
+        return "images/weather-icons/animated/rainy-7.svg"
+    }
+    else if(weather == "Thunderstorm"){
+        return "images/weather-icons/animated/thunder.svg"
+    }
+    else{
+        return "images/weather-icons/animated/weather.svg"
+    }
+}
+
+const weather_request = new XMLHttpRequest();
+weather_request.open("GET",weather_link,true);
+weather_request.onload = function() {
+    let obj = JSON.parse(this.response);
+    let temp = obj.main.temp+"°F";
+    let condition = obj.weather[0].description;
+    condition = condition.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+    let wind = "Wind Speed: "+obj.wind.speed+" MPH";
+
+    console.log(temp+", "+condition+", "+wind)
+
+    document.querySelector(".weather-icon").src = getWeather(condition);
+    document.querySelector(".temp").textContent = temp;
+    document.querySelector(".condition").textContent = condition;
+    document.querySelector(".wind").textContent = wind;
+}
+weather_request.send();
+
