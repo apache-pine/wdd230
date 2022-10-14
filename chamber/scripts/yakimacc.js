db59a70b31fa15e16a56082e340d492828e00586
apache-pine <kylar.sorensen@gmail.com>
2022-10-13 19:54:46 -0700

    updated js, css and html

--- chamber/scripts/yakimacc.js
+++ chamber/scripts/yakimacc.js
@@ -4,13 +4,13 @@ const current_year = now.getFullYear();
 
 document.querySelector(".current-year").textContent = current_year;
 
-let last_modif = new Date(document.lastModified);
+const last_modif = new Date(document.lastModified);
 
 document.querySelector("#last-modif").textContent = `Last Updated: ${last_modif.toLocaleString()}`;
 
-let ham_btn = document.getElementById("ham-btn");
+const ham_btn = document.getElementById("ham-btn");
 
-let primary_nav = document.getElementById("primary-nav");
+const primary_nav = document.getElementById("primary-nav");
 
 function toggleMenu() {
     primary_nav.classList.toggle("open")
@@ -24,3 +24,6 @@ const current_date = document.querySelector(".current-date");
 const full_date = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 
 current_date.innerHTML = `<em>${full_date}</em>`;
+
+const current_day = now.getDay();
+
