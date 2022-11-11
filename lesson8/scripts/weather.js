bae371c9500ed38fd66ddf43bddff03c5ff32f67
apache-pine <kylar.sorensen@gmail.com>
2022-11-11 08:15:54 -0800

    changed case from snake to camel in JS

--- lesson8/scripts/weather.js
+++ lesson8/scripts/weather.js
@@ -1,23 +1,23 @@
-const weather_link = "https://api.openweathermap.org/data/2.5/weather?q=yakima,wa,usa&appid=f2cfbb52b6e01d3767725b983a37e017&units=imperial";
+const weatherLink = "https://api.openweathermap.org/data/2.5/weather?q=yakima,wa,usa&appid=f2cfbb52b6e01d3767725b983a37e017&units=imperial";
 
 let results = null;
-async function getWeather(weather_link) {
-  const response = await fetch(weather_link);
+async function getWeather(weatherLink) {
+  const response = await fetch(weatherLink);
   if (response.ok) {
     const data = await response.json();
     doStuff(data);
   }
 }
 function getWeatherIcon(icon){
-	let icon_url = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    return icon_url;
+	let iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    return iconUrl;
 };
 
 function calcWindChill(t, s) {
   if (t <= 50 && s > 3){
-      const wind_chill = Math.round(
+      const windChill = Math.round(
           35.74 + 0.6215 * t - 35.75 * s **0.16 + 0.4275 * t * s ** 0.16);
-      return wind_chill;
+      return windChill;
   } else {
       return "N/A";
   };
@@ -40,4 +40,4 @@ function doStuff(data) {
     "Wind Chill: "+calcWindChill(temp, windSpeed)+"°F";
   };
 
-getWeather(weather_link);
+getWeather(weatherLink);
