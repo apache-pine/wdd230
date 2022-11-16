926a2251e033f0caf72d6e18aa3e4dd59841df8c
Kylar Sorensen <kylar.sorensen@gmail.com>
2022-11-16 15:04:32 -0800

    fixed the js

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -10,6 +10,21 @@ const currentYear = now.getFullYear();
 
 document.querySelector(".current-year").textContent = currentYear;
 
+const weatherIcon = document.querySelector("#weatherIcon");
+const conditionId = document.querySelector("#condition");
+const temp = document.querySelector("#temp");
+const feelsLike = document.querySelector("#feelsLike");
+const windChill = document.querySelector("#windChill");
+const tempMax = document.querySelector("#tempMax");
+const tempMin = document.querySelector("#tempMin");
+const pressure = document.querySelector("#pressure");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#windSpeed");
+const windDirect = document.querySelector("#windDirect");
+const currentTimeId = document.querySelector("#currentTime");
+const sunrise = document.querySelector("#sunrise");
+const sunset = document.querySelector("#sunset");
+
 let results = null;
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=f2cfbb52b6e01d3767725b983a37e017";
@@ -21,7 +36,7 @@ async function getWeather(url) {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
-            doStuff(data);
+            displayResults(data);
         } else {
             console.log(`Response not OK ${await response.text()}`);
         }
@@ -29,48 +44,34 @@ async function getWeather(url) {
         console.log(`Error: ${error.message}`);
     }
 };
+
 function getWeatherIcon(icon){
 	let icon_url = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     return icon_url;
 };
 
-function doStuff(data) {
+function displayResults(data) {
     results = data;
     let condition = results.weather[0].description;
     condition = condition.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
     let icon = results.weather[0].icon;
-    let weatherIcon = getWeatherIcon(icon);
-    let temp = results.main.temp.toFixed(0);
-    let feelsLike = results.main.feels_like.toFixed(0);
-    let windChill = calcWindChill(results.main.temp, results.wind.speed);
-    let tempMax = results.main.temp_max.toFixed(0);
-    let tempMin = results.main.temp_min.toFixed(0);
-    let pressure = results.main.pressure;
-    let humidity = results.main.humidity;
-    let windSpeed = results.wind.speed;
-    let windDirect = results.wind.deg;
-    let sunrise = new Date(results.sys.sunrise * 1000
-        ).toLocaleTimeString("en-US");
-    let sunset = new Date(results.sys.sunset * 1000
-        ).toLocaleTimeString("en-US");
-
-    document.querySelector("#weatherIcon").src = weatherIcon;
-    document.querySelector("#weatherIcon").alt = condition;
-    document.querySelector("#condition").textContent = condition;
-    document.querySelector("#temp").textContent = temp;
-    document.querySelector("#feelsLike").textContent = feelsLike;
-    document.querySelector("#windChill").textContent = windChill;
-    document.querySelector("#tempMax").textContent = tempMax;
-    document.querySelector("#tempMin").textContent = tempMin;
-    document.querySelector("#pressure").textContent = pressure;
-    document.querySelector("#humidity").textContent = humidity;
-    document.querySelector("#windSpeed").textContent = windSpeed;
-    document.querySelector("#windDirect").textContent = windDirect;
-    document.querySelector("#currentTime").textContent = currentTime;
-    document.querySelector("#sunrise").textContent = sunrise;
-    document.querySelector("#sunset").textContent = sunset;
-    
+    weatherIcon.src = getWeatherIcon(icon);
+    weatherIcon.alt = condition;
+    conditionId.textContent = condition;
+    temp.textContent = Math.round(results.main.temp);
+    feelsLike.textContent = Math.round(results.main.feels_like);
+    windChill.textContent = calcWindChill(results.main.temp, results.wind.speed);
+    tempMax.textContent = Math.round(results.main.temp_max);
+    tempMin.textContent = Math.round(results.main.temp_min);
+    pressure.textContent = results.main.pressure;
+    humidity.textContent = results.main.humidity;
+    windSpeed.textContent = results.wind.speed;
+    windDirect.textContent = results.wind.deg;
+    currentTimeId.textContent = currentTime;
+    sunrise.textContent = new Date(results.sys.sunrise * 1000).toLocaleTimeString("en-US");
+    sunset.textContent = new Date(results.sys.sunset * 1000).toLocaleTimeString("en-US");    
   };
+
 function calcWindChill(t, s) {
   if (t <= 50 && s > 3){
       const wind_chill = Math.round(
