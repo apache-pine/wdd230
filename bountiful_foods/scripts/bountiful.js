61b8d81c111c37c6851a9a47737097d2df18bce5
Kylar Sorensen <kylar.sorensen@gmail.com>
2022-12-09 18:39:01 -0800

    added files and js to bountiful file

---
+++ bountiful_foods/scripts/bountiful.js
@@ -0,0 +1,24 @@
+const now = new Date();
+
+const currentYear = now.getFullYear();
+
+const currentDay = now.getDay();
+
+const theHour = now.getHours();
+
+document.querySelector(".currentYear").textContent = currentYear;
+
+const lastModif = new Date(document.lastModified);
+
+document.querySelector(".lastModif").textContent = `Last Updated: ${lastModif.toLocaleString()}`;
+
+const hamBtn = document.querySelector("#hamBtn");
+
+const primaryNav = document.querySelector("#primaryNav");
+
+function toggleMenu() {
+    primaryNav.classList.toggle("open")
+    hamBtn.classList.toggle("open")
+};
+
+hamBtn.onclick = toggleMenu;
